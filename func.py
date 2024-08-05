4825d5d9919219d325ab277a6a4e1ef263d367a8
Tayler Duncan <tduncan@c3biz.com>
2024-08-05 17:49:05 +0000

    updated requirements

--- func.py
+++ func.py
@@ -10,6 +10,7 @@ import requests
 import base64
 from PIL import Image
 import io
+from fdk import response
 
 known_face_encodings = []
 known_face_names = []
@@ -17,32 +18,64 @@ bucket_url = 'https://objectstorage.us-chicago-1.oraclecloud.com/p/j8d1ZFSVLZnXb
 
 def handler(ctx, data: io.BytesIO=None):    
     try:
-        r = requests.get(bucket_url)
-        objects = r.json()['objects']
-    except Exception as requestError:
-        return jsonify({"RequestError": str(requestError)}), 400
-
-    for object in objects:
         try:
-            bucketImage = urllib.request.urlopen(bucket_url + object['name'].replace(' ', '%20'))
-            arr = np.asarray(bytearray(bucketImage.read()), dtype=np.uint8)
-            img = cv2.imdecode(arr, -1)
-            rgb_img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_encoding = face_recognition.face_encodings(rgb_img2)[0]
-            known_face_encodings.append(img_encoding)
-            known_face_names.append(object['name'])
-        except Exception as e:
-            return jsonify({"EncodingError": str(e)}), 400
+            r = requests.get(bucket_url)
+            objects = r.json()['objects']
+        except Exception as requestError:
+            # return jsonify({"RequestError": str(requestError)}), 400
+            return response.Response(
+                ctx, response_data=json.dumps(
+                    {"RequestError": str(requestError)}),
+                headers={"Content-Type": "application/json"}
+            )
 
-    try:
-        np.save('known_face_encodings.npy', known_face_encodings)
-    except Exception as faceEncodingfileError:
-        return jsonify({"FaceEncodingFileError": str(faceEncodingfileError)}), 400
-    try:
-        with open("known_face_names.txt", "w") as txt_file:
-            for line in known_face_names:
-                txt_file.write(line + "\n")
-    except Exception as faceNamefileError:
-        return jsonify({"FaceNameFileError": str(faceNamefileError)}), 400
-    response = jsonify({"status": "completed"})
-    return response
+        for object in objects:
+            try:
+                bucketImage = urllib.request.urlopen(bucket_url + object['name'].replace(' ', '%20'))
+                arr = np.asarray(bytearray(bucketImage.read()), dtype=np.uint8)
+                img = cv2.imdecode(arr, -1)
+                rgb_img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img_encoding = face_recognition.face_encodings(rgb_img2)[0]
+                known_face_encodings.append(img_encoding)
+                known_face_names.append(object['name'])
+            except Exception as e:
+                # return jsonify({"EncodingError": str(e)}), 400
+                return response.Response(
+                    ctx, response_data=json.dumps(
+                        {"EncodingError": str(e)}),
+                    headers={"Content-Type": "application/json"}
+                )
+
+        try:
+            np.save('known_face_encodings.npy', known_face_encodings)
+        except Exception as faceEncodingfileError:
+            # return jsonify({"FaceEncodingFileError": str(faceEncodingfileError)}), 400
+            return response.Response(
+                ctx, response_data=json.dumps(
+                    {"FaceEncodingFileError": str(faceEncodingfileError)}),
+                headers={"Content-Type": "application/json"}
+            )
+        try:
+            with open("known_face_names.txt", "w") as txt_file:
+                for line in known_face_names:
+                    txt_file.write(line + "\n")
+        except Exception as faceNamefileError:
+            # return jsonify({"FaceNameFileError": str(faceNamefileError)}), 400
+            return response.Response(
+                ctx, response_data=json.dumps(
+                    {"FaceNameFileError": str(faceNamefileError)}),
+                headers={"Content-Type": "application/json"}
+            )
+        # response = jsonify({"status": "completed"})
+        # return response
+        return response.Response(
+            ctx, response_data=json.dumps(
+                {"status": "completed"}),
+            headers={"Content-Type": "application/json"}
+        )
+    except Exception as genErr:
+        return response.Response(
+            ctx, response_data=json.dumps(
+                {"genErr": str(genErr)}),
+            headers={"Content-Type": "application/json"}
+        )
